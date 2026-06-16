f36d2306c063fafcb9c2985501649faefec96c3a
HenzaDB <chen.henry96@gmail.com>
2026-06-16 20:50:59 +1000

    Fix PostgreSQL connection parameter

--- data_explore.py
+++ data_explore.py
@@ -9,7 +9,7 @@ conn = psycopg2.connect(
     host = os.getenv("DB_HOST"),
     port = os.getenv("DB_PORT"),
     dbname = os.getenv("DB_NAME"),
-    dbuser = os.genenv("DB_USER"),
+    user = os.getenv("DB_USER"),
     password = os.getenv("DB_PASSWORD")
 )
 
